6898a3082a5189cdd4925386e7ca7c8dfefe41c6
JorUge <joruge@gmail.com>
2022-11-18 19:17:04 -0300

    Update

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,10 +5,10 @@ fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let mut guess = String::new(); // Criando uma varíavel mutável que irá armazenar uma nova instância vazia do tipo String.
 
     io::stdin()
-       .read_line(&mut guess)
+       .read_line(&mut guess)  // Precisamos colocar "&mut guess" por ser uma refência mutável. 
        .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
